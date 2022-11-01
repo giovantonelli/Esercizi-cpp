c2813abe15def5c88ed7a5eee4ef5db086889029
heroeminecratti <31672217+heroeminecratti@users.noreply.github.com>
2022-11-01 19:16:11 +0100

    /

--- Pag137n18.cpp
+++ Pag137n18.cpp
@@ -1,19 +1,30 @@
+//Inserito il prezzo di un biglietto per il cinema e la relativa categoria di appartenenza
+//(S=studenti, P=pensionati, H=diversamente abili, D=disoccupati) calcolarne il prezzo scontato
 #include <iostream>
 using namespace std;
 
 int main() {
-    int peso,costo;
-    cout<<"Inserisci il peso del pacco da spedire: ";
-    cin>>peso;
-    if (peso<=3) {
-        costo=5;
-    } else {
-        if (peso<=10) {
-            costo=8;
-        } else {
-            costo=10;
-        }
+    float prezzo;
+    char categoria;
+    cout<<"Inserire il prezzo e la lettera di categoria appartenente: "<<endl;
+    cin>>prezzo>>categoria;
+    switch(categoria) {
+        case 'S':
+            prezzo*=0.5;    //50% di sconto
+            break;
+        case 'P':
+            prezzo*=0.75;   //25% di sconto
+            break;
+        case 'H':
+            prezzo=0;       //gratis
+            break;
+        case 'D':
+            prezzo*=0.25;   //75% di sconto
+            break;
+        default:
+            cout<<"Non hai diritto ad alcuno sconto!";
+            break;
     }
-    cout<<"Il costo della spedizione e' pari a "<<costo<<" euro"<<endl;
+    cout<<"Il prezzo del biglietto e' "<<prezzo<<endl;
     return 0;  
 }
