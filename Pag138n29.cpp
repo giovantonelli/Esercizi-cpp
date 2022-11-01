2fbfe52f84dc33756b82921bbc1db921cbe1712f
heroeminecratti <31672217+heroeminecratti@users.noreply.github.com>
2022-11-01 18:45:29 +0100

    /

---
+++ Pag138n29.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int N, tempo,min=1000;
+    string nome,categoria;
+    cout<<"Inserisci il numero di studenti partecipanti"<<endl;
+    cin>>N;
+    for (int i=1; i<=N; i++) {
+    cin>>nome>>categoria>>tempo;
+    if (categoria=="allievi") {
+        if (tempo<min) {
+            min=tempo;
+            }
+        }
+    }
+    cout<<tempo<<endl;
+    return 0;
+}
