1705186320031eefde411ae538e3ba9cf5374c47
heroeminecratti <31672217+heroeminecratti@users.noreply.github.com>
2023-05-02 17:55:43 +0200

    /

--- Libro/Pag140n59.cpp
+++ Libro/Pag140n59.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int main() {
     int v[10];
     int* v_punt=v;
-    cout<<"Inserisci 10 numeri interi"<<endl;
     for (int i=0; i<10; i++) {
-        cin>>v[i];
+        cout<<"Inserisci il valore dell'elemento "<<i<<": ";
+        cin>>*(v_punt+i);
     }
+    cout << "I valori dell'array sono:"<<endl;
     for (int i=0; i<10; i++) {
-        cout<<*(v_punt+i)<<endl;
+        cout<<*(v_punt+i)<<" ";
     }
+    cout<<endl;
     system("PAUSE");
     return 0;
 }
