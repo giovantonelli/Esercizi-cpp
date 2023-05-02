ac8d4fa272ddeebef27bab2df4046d8168abc895
heroeminecratti <31672217+heroeminecratti@users.noreply.github.com>
2023-05-02 17:07:07 +0200

    /

--- Libro/Pag140n57.cpp
+++ Libro/Pag140n57.cpp
@@ -1,20 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-	int a=7,b=13;
-	int* p1;
-	int* p2;
-	p1=&a;
-	p2=&b;
-	cout<<"valore di a "<<a<<endl;
-	cout<<"valore di b "<<b<<endl;
-	cout<<"indirizzo di a "<<&a<<endl;
-	cout<<"indirizzo di b "<<&b<<endl;
-	cout<<"valore del puntatore p1 "<<p1<<endl;
-	cout<<"valore del puntatore p2 "<<p2<<endl;
-	cout<<"indirizzo del puntatore p1 "<<&p1<<endl;
-	cout<<"indirizzo del puntatore p2 "<<&p2<<endl;
-	system("PAUSE");
-	return 0;
-}
 
+int main() {
+    int a=5,b=10;
+    int* puntA=&a;
+    int* puntB=&b;
+    cout<<a<<'\t'<<b<<endl;
+    cout<<&a<<'\t'<<&b<<endl;
+    cout<<puntA<<'\t'<<puntB<<endl;
+    cout<<&puntA<<'\t'<<&puntB<<endl;
+    system("PAUSE");
+    return 0;
+}
