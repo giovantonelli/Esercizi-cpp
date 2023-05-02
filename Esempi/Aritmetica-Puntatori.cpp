a31eda959a4cf79d5f0bb7ed27fccda35a064a9f
Giovanni Antonelli <31672217+giovantonelli@users.noreply.github.com>
2023-05-02 18:35:49 +0200

    /

--- Esempi/Aritmetica-Puntatori.cpp
+++ Esempi/Aritmetica-Puntatori.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
+
 int main() {
     double v[5]={11.54,15,6.9,89.5,67};
     double* punt=v;
-	cout<<punt<<endl;
-	cout<<punt+1<<endl;
-	cout<<*punt+1<<endl;
-	cout<<*(punt+1)<<endl;
+	cout<<"Indirizzo di memoria di v[0]: "<<punt<<endl; // &v[0]
+	cout<<"Indirizzo di memoria di v[1]: "<<punt+1<<endl; // &v[1]
+	cout<<"Valore di v[0] incrementato di 1: "<<*punt+1<<endl; // v[0]+1
+	cout<<"Valore di v[1]: "<<*(punt+1)<<endl; // v[1]
     system("PAUSE");
     return 0;
 }
