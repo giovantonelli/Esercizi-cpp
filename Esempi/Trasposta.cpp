7b87e06d95e45767afbba5bad1ac05cd9410adf0
heroeminecratti <31672217+heroeminecratti@users.noreply.github.com>
2023-05-02 15:43:40 +0200

    /

--- Esempi/Trasposta.cpp
+++ Esempi/Trasposta.cpp
@@ -1,20 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int m[3][3];
-    int m_trasposta[3][3];
-    for (int i=1;i<=3;i++){
-		for (int j=1;j<=3;j++){
-			cin>>m[i][j];
-		}
-	}
-    for (int i=1;i<=3;i++){
-		for (int j=1;j<=3;j++){
-			m_trasposta[i][j]=m[j][i];
-            cout<<m_trasposta[i][j];
-            }
-	}
-    system("PAUSE");
+int main() {
+    int matrice[3][3];
+    int trasposta[3][3];
+
+    // leggi la matrice da input
+    cout << "Inserisci i valori della matrice (3x3):" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matrice[i][j];
+        }
+    }
+
+    // calcola la trasposta
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            trasposta[i][j] = matrice[j][i];
+        }
+    }
+
+    // stampa la matrice originale
+    cout << "Matrice originale:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrice[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // stampa la matrice trasposta
+    cout << "Matrice trasposta:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << trasposta[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
