689625c56e502815a6ff9cb7c4f0423d0e854486
Giovanni Antonelli <31672217+giovantonelli@users.noreply.github.com>
2023-05-03 07:41:42 +0200

    /

--- Libro/Pag140n53.cpp
+++ Libro/Pag140n53.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 struct Abitazione{
-        string via;
-        int CAP;
-        string Comune;
-    };
+    string via;
+    int CAP;
+    string Comune;
+};
 
-    struct Persona {
-        int matricola;
-        Abitazione indirizzo;
-        string datanascita;
-        string codfiscale;
-        bool coniugato;
-    };
+struct Persona {
+    int matricola;
+    Abitazione indirizzo;
+    string datanascita;
+    string codfiscale;
+    bool coniugato;
+};
 
 int main() {
     Persona dipendente;
